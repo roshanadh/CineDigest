4c65c3c90782ce22ec0dee555745f20cbbd7f3b2
Roshan Adhikari <nahsorad@gmail.com>
2019-07-31 23:10:29 +0545

    Read param from SignInScreen

--- src/components/MainScreen.js
+++ src/components/MainScreen.js
@@ -1,8 +1,28 @@
 import React, {Component} from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, StyleSheet} from 'react-native';
 
 export default class MainScreen extends Component {
 	render() {
-		return (<View><Text>Welcome!</Text></View>);
+        const { navigation } = this.props;
+        const emailId = navigation.getParam('emailId', 'defaultEmail');
+		return (
+            <View style={styles.container}>
+                <Text style={styles.welcomeHeader}>Welcome, {emailId}!</Text>
+            </View>
+        );
 	}
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100%',
+        padding: 50,
+    },
+	welcomeHeader: {
+		fontSize: 35,
+    },
+});
