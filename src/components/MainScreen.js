6fb11e00696b274a0b6a3cadc8accf7459d3f374
Roshan Adhikari <nahsorad@gmail.com>
2019-08-01 21:01:59 +0545

    Style tabBar

--- src/components/MainScreen.js
+++ src/components/MainScreen.js
@@ -6,24 +6,35 @@ import {createBottomTabNavigator, createAppContainer} from 'react-navigation';
 import MoviesScreen from './MoviesScreen.js';
 import ShowsScreen from './ShowsScreen.js';
 
-const movieIcon = <Icon name="film" size={30} color="#696969" />;
-const showIcon = <Icon name="television" size={30} color="#696969" />;
-
-const AppTabNavigator = createBottomTabNavigator({
-    MoviesScreen: {
-        screen: MoviesScreen,
-        navigationOptions: {
-            tabBarLabel: 'Movies',
-            tabBarIcon: movieIcon,
+const AppTabNavigator = createBottomTabNavigator(
+    {
+        MoviesScreen: {
+            screen: MoviesScreen,
+            navigationOptions: {
+                tabBarLabel: 'Movies',
+                tabBarIcon: ({tintColor}) => (
+                    <Icon name="film" size={30} color={tintColor} />
+                ),
+            },
+        },
+        ShowsScreen: {
+            screen: ShowsScreen,
+            navigationOptions: {
+                tabBarLabel: 'Shows',
+                tabBarIcon: ({tintColor}) => (
+                    <Icon name="television" size={30} color={tintColor} />
+                ),
+            },
         },
     },
-	ShowsScreen: {
-        screen: ShowsScreen,
-        navigationOptions: {
-            tabBarLabel: 'Shows',
-            tabBarIcon: showIcon,
+    {
+        // router config, navigationOptions {for whole tabBar},
+        // tabBarOptions go here
+        tabBarOptions: {
+            activeTintColor: 'red',
+            inactiveTintColor: 'grey',
         },
     },
-});
+);
 
 export default createAppContainer(AppTabNavigator);
