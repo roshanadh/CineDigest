c90d2b5610524920d835981733b06c9a0e3b5b4a
Roshan Adhikari <nahsorad@gmail.com>
2019-08-01 22:18:31 +0545

    Add search option

--- src/components/ShowsScreen.js
+++ src/components/ShowsScreen.js
@@ -1,13 +1,78 @@
 import React, {Component} from 'react';
-import { Text, View } from 'react-native';
+import { 
+	Text,
+	View,
+	TextInput,
+	StyleSheet,
+	TouchableOpacity,
+} from 'react-native';
 
 export default class ShowsScreen extends Component {
+	constructor(props, context) {
+		super(props, context);
+		this.state = {
+			searchQuery: '',
+		};
+	}
+
+    searchFieldChangedHandler = (newQuery) => {
+		this.setState({
+			searchQuery: newQuery,
+		});
+	};
+
+	searchBtnPressedHandler = () => {
+		alert(this.state.searchQuery);
+	};
+
     render() {
 		return (
-			<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-				<Text>Shows!</Text>
+			<View style={styles.container}>
+				<View style={styles.searchWrapper}>
+					<TextInput placeholder="Search a TV show"
+						style={styles.searchTextInput}
+						onChangeText={this.searchFieldChangedHandler} />
+
+					<TouchableOpacity style={styles.searchBtn}
+						onPress={this.searchBtnPressedHandler}>
+						<Text>Search</Text>
+					</TouchableOpacity>
+				</View>
 			</View>
 		);
 	}
 }
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		flexDirection: 'column',
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	searchWrapper: {
+		margin: 10,
+		flexDirection: 'row',
+		flex: 1,
+		justifyContent: 'flex-start',
+		alignItems: 'flex-start',
+	},
+	searchTextInput: {
+		marginRight: 5,
+		borderColor: '#010101',
+		borderRadius: 5,
+		borderWidth: 1,
+		paddingLeft: 20,
+		paddingRight: 20,
+		width: '78%',
+	},
+	searchBtn: {
+		alignItems: 'center',
+		borderWidth: 1,
+		borderRadius: 5,
+		padding: 15,
+		minHeight: '6%',
+		width: '18%',
+	},
+});
 
