5e23b0cbecb42980589efe05b72270619367d0f8
Roshan Adhikari <nahsorad@gmail.com>
2019-08-01 20:48:09 +0545

    Add icons for bottom tabs

--- src/components/MainScreen.js
+++ src/components/MainScreen.js
@@ -1,12 +1,29 @@
 import {Text, View, StyleSheet} from 'react-native';
+import React from 'react';
+import Icon from 'react-native-vector-icons/FontAwesome';
 import {createBottomTabNavigator, createAppContainer} from 'react-navigation';
 
 import MoviesScreen from './MoviesScreen.js';
 import ShowsScreen from './ShowsScreen.js';
 
+const movieIcon = <Icon name="film" size={30} color="#696969" />;
+const showIcon = <Icon name="television" size={30} color="#696969" />;
+
 const AppTabNavigator = createBottomTabNavigator({
-    MoviesScreen,
-	ShowsScreen,
+    MoviesScreen: {
+        screen: MoviesScreen,
+        navigationOptions: {
+            tabBarLabel: 'Movies',
+            tabBarIcon: movieIcon,
+        },
+    },
+	ShowsScreen: {
+        screen: ShowsScreen,
+        navigationOptions: {
+            tabBarLabel: 'Shows',
+            tabBarIcon: showIcon,
+        },
+    },
 });
 
 export default createAppContainer(AppTabNavigator);
