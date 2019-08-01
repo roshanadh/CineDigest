ecc1495432b7b420ad209cf215080d13f0762870
Roshan Adhikari <nahsorad@gmail.com>
2019-08-01 22:01:33 +0545

    Style searchMovie TextInput

--- src/components/MoviesScreen.js
+++ src/components/MoviesScreen.js
@@ -1,13 +1,50 @@
 import React, {Component} from 'react';
-import { Text, View } from 'react-native';
+import { 
+	Text,
+	View,
+	TextInput,
+	StyleSheet,
+	TouchableOpacity,
+} from 'react-native';
 
 export default class MoviesScreen extends Component {
     render() {
 		return (
-			<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-				<Text>Movies!</Text>
+			<View style={styles.container}>
+				<TextInput placeholder="Search movie"
+					style={styles.searchTextInput} />
 			</View>
 		);
 	}
 }
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		flexDirection: 'column',
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	searchTextInput: {
+		borderColor: '#010101',
+		borderRadius: 5,
+		borderWidth: 1,
+		marginBottom: 10,
+		paddingLeft: 20,
+		paddingRight: 20,
+	},
+	searchBtn: {
+		marginTop: 20,
+		flex: 1,
+		flexDirection: 'row',
+		justifyContent: 'center',
+		alignItems: 'center',
+		alignSelf: 'center',
+		borderWidth: 1,
+		borderRadius: 50,
+		padding: 15,
+		minHeight: '8%',
+		width: '5%',
+	},
+});
 
