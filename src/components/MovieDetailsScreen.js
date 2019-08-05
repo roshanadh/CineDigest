e95e5500bc68819113d68e983ed039f8a73ab3d7
Roshan Adhikari <nahsorad@gmail.com>
2019-08-05 19:24:32 +0545

    Add new MovieDetailsScreen

---
+++ src/components/MovieDetailsScreen.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import {
+    Text,
+    View,
+} from 'react-native';
+
+export default class MovieDetails extends Component {
+    render() {
+        return (
+            <View>
+                <Text>Hello World!</Text>
+            </View>
+        );
+    }
+}
