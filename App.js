0fa8f03326a60efbcc0648593e0269eff5b804f9
Roshan Adhikari <nahsorad@gmail.com>
2019-08-08 20:07:48 +0545

    Create App entry point based on sign-in state

--- App.js
+++ App.js
@@ -1,8 +1,32 @@
 import React, {Component} from 'react';
-import SignedOut from './src/components/router';
+import { isSignedIn } from './src/auth/auth';
+import {createRootNavigator} from './src/components/router';
 
 export default class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			signedIn: false,
+			checkedSignIn: false,
+		};
+	}
+
+	componentDidMount() {
+	isSignedIn()
+		.then(res => this.setState({ signedIn: res, checkedSignIn: true }))
+		.catch(err => alert('An error occurred'));
+	}
+
 	render() {
-		return <SignedOut />;
+		const { checkedSignIn, signedIn } = this.state;
+
+		// If we haven't checked AsyncStorage yet,
+		// don't render anything (better ways to do this)
+		if (!checkedSignIn) {
+			return null;
+		}
+		const Layout = createRootNavigator(signedIn);
+		return <Layout />;
 	}
 }
