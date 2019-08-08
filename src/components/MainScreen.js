37a5afe993fd582b5068af68e8b4a33d633190fa
Roshan Adhikari <nahsorad@gmail.com>
2019-08-08 23:50:08 +0545

    Fix navigation from main screen

--- src/components/MainScreen.js
+++ src/components/MainScreen.js
@@ -1,74 +1,38 @@
 import React from 'react';
-
-import MovieIcon from 'react-native-vector-icons/FontAwesome';
-import TVIcon from 'react-native-vector-icons/FontAwesome';
-import SettingsIcon from 'react-native-vector-icons/SimpleLineIcons';
+import {
+    TouchableOpacity,
+    Text,
+    StyleSheet,
+    View,
+} from 'react-native';
 
 import {
-    createMaterialTopTabNavigator,
     createAppContainer,
+    createStackNavigator,
 } from 'react-navigation';
 
-import MoviesListsScreen from './MoviesListsScreen';
-import ShowsListsScreen from './ShowsListsScreen';
-import SettingsScreen from './SettingsScreen';
+import MovieDetailsScreen from './MovieDetailsScreen';
+import ShowDetailsScreen from './ShowDetailsScreen';
+import SeasonDetailsScreen from './SeasonDetailsScreen';
+import TabNavigator from './TabNavigator';
 
-const AppTabNavigator = createMaterialTopTabNavigator(
-    {
-        MoviesListsScreen: {
-            screen: MoviesListsScreen,
-            navigationOptions: {
-                tabBarLabel: 'Movies',
-                tabBarIcon: ({tintColor}) => (
-                    <MovieIcon name="film" size={20} color={tintColor} />
-                ),
-            },
+const StackNavigator = createAppContainer(new createStackNavigator({
+    TabNavigator: {
+        screen: TabNavigator,
+        navigationOptions: {
+            title: 'Cine Digest',
+            headerRight: (
+                <View>
+                <TouchableOpacity onPress={() => alert('This is a button!')} style={{marginRight: 20,}}>
+                    <Text>Click!</Text>
+                </TouchableOpacity>
+                </View>
+            ),
         },
-        ShowsListsScreen: {
-            screen: ShowsListsScreen,
-            navigationOptions: {
-                tabBarLabel: 'Shows',
-                tabBarIcon: ({tintColor}) => (
-                    <TVIcon name="television" size={20} color={tintColor} />
-                ),
-            },
-        },
-        SettingsScreen: {
-            screen: SettingsScreen,
-            navigationOptions: {
-                tabBarLabel: 'Settings',
-                tabBarIcon: ({tintColor}) => (
-                    <SettingsIcon name="settings" size={21} color={tintColor} />
-                ),
-            },
-        },
-
     },
-    {
-        // router config, navigationOptions {for whole tabBar},
-        // tabBarOptions go here
-        tabBarOptions: {
-            inactiveTintColor: '#24252a',
-            labelStyle: {
-                fontSize: 12,
-            },
-            indicatorStyle: {
-                backgroundColor: '#fff',
-            },
-            showIcon: true,
-            showLabel: false,
-            style: {
-                backgroundColor: '#22a7f0',
-                borderTopWidth: 1,
-                borderTopColor: '#bbb',
-                height: 60,
-                paddingTop: 5,
-                paddingBottom: 5,
-            },
-        },
-        tabBarPosition: 'bottom',
-        lazy: true,
-    },
-);
+    MovieDetailsScreen,
+    ShowDetailsScreen,
+    SeasonDetailsScreen,
+}));
 
-export default createAppContainer(AppTabNavigator);
+export default StackNavigator;
