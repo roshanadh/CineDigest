a62ceab906221ad31079a8f55e90dfb4f367ed29
Roshan Adhikari <nahsorad@gmail.com>
2019-08-08 23:50:32 +0545

    Create new Tab Navigator to be nested in stack

---
+++ src/components/TabNavigator.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import {
+    createAppContainer,
+    createMaterialTopTabNavigator,
+} from 'react-navigation';
+
+import MovieIcon from 'react-native-vector-icons/FontAwesome';
+import TVIcon from 'react-native-vector-icons/FontAwesome';
+import SettingsIcon from 'react-native-vector-icons/SimpleLineIcons';
+
+import MoviesListsScreen from './MoviesListsScreen';
+import ShowsListsScreen from './ShowsListsScreen';
+import SettingsScreen from './SettingsScreen';
+
+const TabNavigator = createMaterialTopTabNavigator(
+    {
+        MoviesListsScreen: {
+            screen: MoviesListsScreen,
+            navigationOptions: {
+                tabBarLabel: 'Movies',
+                tabBarIcon: ({tintColor}) => (
+                    <MovieIcon name="film" size={20} color={tintColor} />
+                ),
+            },
+        },
+        ShowsListsScreen: {
+            screen: ShowsListsScreen,
+            navigationOptions: {
+                tabBarLabel: 'Shows',
+                tabBarIcon: ({tintColor}) => (
+                    <TVIcon name="television" size={20} color={tintColor} />
+                ),
+            },
+        },
+        SettingsScreen: {
+            screen: SettingsScreen,
+            navigationOptions: {
+                tabBarLabel: 'Settings',
+                tabBarIcon: ({tintColor}) => (
+                    <SettingsIcon name="settings" size={21} color={tintColor} />
+                ),
+            },
+        },
+
+    },
+    {
+        // router config, navigationOptions {for whole tabBar},
+        // tabBarOptions go here
+        tabBarOptions: {
+            inactiveTintColor: '#24252a',
+            labelStyle: {
+                fontSize: 12,
+            },
+            indicatorStyle: {
+                backgroundColor: '#fff',
+            },
+            showIcon: true,
+            showLabel: false,
+            style: {
+                backgroundColor: '#22a7f0',
+                borderTopWidth: 1,
+                borderTopColor: '#bbb',
+                height: 60,
+                paddingTop: 5,
+                paddingBottom: 5,
+            },
+        },
+        tabBarPosition: 'bottom',
+        lazy: true,
+    },
+);
+
+export default createAppContainer(TabNavigator);
