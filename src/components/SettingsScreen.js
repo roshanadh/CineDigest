81241c9f8d029d94f7366f023753941aa94f02b6
Roshan Adhikari <nahsorad@gmail.com>
2019-08-08 20:09:24 +0545

    Export class from SettingsScreen

--- src/components/SettingsScreen.js
+++ src/components/SettingsScreen.js
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {
     Text,
     View,
     StyleSheet,
     Button,
 } from 'react-native';
+import {onSignOut} from '../auth/auth';
 
-export default ({ navigation }) => (
-    <View style={{paddingVertical: 20}}>
-        <Button
-          title="Sign out"
-          onPress={() => navigation.navigate('SignInScreen')}
-        />
-    </View>
-  );
+export default class SettingsScreen extends Component {
+    render() {
+        return (
+            <View style={{paddingVertical: 20}}>
+                <Button
+                    title="Sign out"
+                    onPress={() => onSignOut().then(() => this.props.navigation.navigate('SignedOut'))}
+                />
+            </View>
+        );
+    }
+}
