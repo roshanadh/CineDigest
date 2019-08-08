36ac005d3c53fdd1cd9dd5a3eb67fdb6700b7790
Roshan Adhikari <nahsorad@gmail.com>
2019-08-08 20:22:24 +0545

    Style SignUpScreen

--- src/components/SignUpScreen.js
+++ src/components/SignUpScreen.js
@@ -3,18 +3,40 @@ import {
     Text,
     View,
     StyleSheet,
-    Button,
+    TouchableOpacity,
 } from 'react-native';
 
 export default class SignUpScreen extends Component {
     render() {
         return (
-            <View style={{ paddingVertical: 20 }}>
-                <Button
-                    title="Sign In"
-                    onPress={() => this.props.navigation.navigate('SignIn')}
-                />
+            <View style={styles.container}>
+                <TouchableOpacity style={styles.signUpBtn}
+                    onPress={() => this.props.navigation.navigate('SignIn')}>
+                    <Text style={styles.btnText}>Sign-up</Text>
+                </TouchableOpacity>
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 20,
+		flex: 1,
+		flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    signUpBtn: {
+        marginTop: 20,
+		alignItems: 'center',
+		alignSelf: 'center',
+		borderRadius: 50,
+        padding: 15,
+        width: '50%',
+		backgroundColor: '#22a7f0',
+    },
+    btnText: {
+		color: '#fff',
+	},
+});
