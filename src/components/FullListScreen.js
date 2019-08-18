6923dd071cbbfe5d643a4fc00529a141d3c3c0ce
Roshan Adhikari <nahsorad@gmail.com>
2019-08-18 18:50:49 +0545

    Add FullListScreen

---
+++ src/components/FullListScreen.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import {
+    Text,
+    View,
+    StyleSheet,
+} from 'react-native';
+
+export default class FullListScreen extends Component {
+    render() {
+        return (
+            <View>
+                <Text>Hello World!</Text>
+            </View>
+        );
+    }
+}
