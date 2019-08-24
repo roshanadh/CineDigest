b067aea7331d139a3181f09496599a4b7acddc98
Roshan Adhikari <nahsorad@gmail.com>
2019-08-24 12:54:56 +0545

    Fix haste season details load

--- src/components/SeasonDetailsScreen.js
+++ src/components/SeasonDetailsScreen.js
@@ -6,6 +6,7 @@ import {
 	StyleSheet,
 	Image,
 	ImageBackground,
+	ActivityIndicator,
 } from 'react-native';
 
 import Icon from 'react-native-vector-icons/FontAwesome';
@@ -38,6 +39,7 @@ export default class SeasonDetailsScreen extends Component {
 			episodeOverviews: [],
 			episodeVoteAverages: [],
 			episodeVoteCounts: [],
+			contentJsx: <ActivityIndicator size="large" color="#22a7f0" style={styles.indicator} />,
 		};
         this.noPoster = false;
 		this.monthNames = ['January', 'February', 'March', 'April', 'May', 'June',
@@ -47,118 +49,77 @@ export default class SeasonDetailsScreen extends Component {
 		this.seasonNo = this.props.navigation.getParam('seasonNo', 1);
 		this.seasonName = this.props.navigation.getParam('seasonName', 'Season Details');
 		this.showName = this.props.navigation.getParam('showName', '');
-		this.fetchSeasonDetails(this.titleId, this.seasonNo);
-	}
 
-	fetchSeasonDetails = (titleId, seasonNo) => {
-		fetch(`https://api-cine-digest.herokuapp.com/api/v1/gets/${titleId}/${seasonNo}`)
-			.then(response => response.json())
-			.then(jsonResponse => {
-				this.episodes = [];
-				this.episodeAirDates = [];
-				this.episodeNames = [];
-				this.episodeOverviews = [];
-				this.episodeVoteAverages = [];
-				this.episodeVoteCounts = [];
-				this.noPoster = jsonResponse.poster_path !== null ? false : true;
-				for (let i = 0; i < jsonResponse.episodes.length; ++i) {
-					this.episodes.push(jsonResponse.episodes[i].episode_number);
-					this.episodeAirDates.push(jsonResponse.episodes[i].air_date);
-					this.episodeNames.push(jsonResponse.episodes[i].name);
-					this.episodeOverviews.push(jsonResponse.episodes[i].overview);
-					this.episodeVoteAverages.push(jsonResponse.episodes[i].vote_average);
-					this.episodeVoteCounts.push(jsonResponse.episodes[i].vote_count);
-				}
-				this.setState(
-					{
-						posterPath: `https://image.tmdb.org/t/p/original/${jsonResponse.poster_path}`,
-						seasonNo: jsonResponse.season_number,
-						airDate: jsonResponse.air_date,
-						episodes: this.episodes,
-						episodeAirDates: this.episodeAirDates,
-						episodeNames: this.episodeNames,
-						episodeOverviews: this.episodeOverviews,
-						episodeVoteAverages: this.episodeVoteAverages,
-						episodeVoteCounts: this.episodeVoteCounts,
-					}
-				);
+		this.initScreen = () => {
+			let posterJsx = this.noPoster === false ?
+				<Image source={{ uri: this.state.posterPath }}
+					style={styles.posterPath}
+					resizeMode="contain" /> : null;
 
-			});
-	};
+			let seasonNameJsx = this.seasonName !== null ?
+				<Text style={styles.seasonName}>
+					{this.seasonName}
+				</Text> : null;
 
-	render() {
-		let posterJsx = this.noPoster === false ?
-            <Image source={{uri: this.state.posterPath}}
-                style={styles.posterPath}
-				resizeMode="contain"/> : null;
+			let showNameJsx = this.showName !== '' ?
+				<Text style={styles.showName}>
+					{this.showName}
+				</Text> : null;
 
-		let seasonNameJsx = this.seasonName !== null ?
-			<Text style={styles.seasonName}>
-				{this.seasonName}
-			</Text> : null;
-
-		let showNameJsx = this.showName !== '' ?
-			<Text style={styles.showName}>
-				{this.showName}
-			</Text> : null;
-
-		let airDateJsx = this.state.airDate !== null ?
-			<Text style={styles.text}>
-				Aired on
+			let airDateJsx = this.state.airDate !== null ?
+				<Text style={styles.text}>
+					Aired on
 				{' ' + this.monthNames[new Date(this.state.airDate).getMonth()]}
-				{' ' + this.state.airDate.slice(-2)}, {' ' + this.state.airDate.slice(0, 4)}
-			</Text> : null;
+					{' ' + this.state.airDate.slice(-2)}, {' ' + this.state.airDate.slice(0, 4)}
+				</Text> : null;
 
-		let episodesJsx = [];
+			let episodesJsx = [];
 
-		for (let i = 0; i < this.state.episodes.length; ++i) {
-			episodesJsx.push(
-				<View style={styles.episodeContainer}>
-					<Text style={styles.episodeNumber}>
-						Episode {i + 1}
-					</Text>
-					{
-						this.state.episodeNames[i] !== null ?
-						<Text style={styles.episodeHeader}>
-							{this.state.episodeNames[i]}
-						</Text> : null
-					}
-					{
-						this.state.episodeAirDates[i] !== null ?
-							( new Date(this.state.episodeAirDates[i]) > new Date() ?
-								<Text style={styles.airDate}>
-									Airs on
+			for (let i = 0; i < this.state.episodes.length; ++i) {
+				episodesJsx.push(
+					<View style={styles.episodeContainer}>
+						<Text style={styles.episodeNumber}>
+							Episode {i + 1}
+						</Text>
+						{
+							this.state.episodeNames[i] !== null ?
+								<Text style={styles.episodeHeader}>
+									{this.state.episodeNames[i]}
+								</Text> : null
+						}
+						{
+							this.state.episodeAirDates[i] !== null ?
+								(new Date(this.state.episodeAirDates[i]) > new Date() ?
+									<Text style={styles.airDate}>
+										Airs on
 									{' ' + this.monthNames[new Date(this.state.episodeAirDates[i]).getMonth()]}
-									{' ' + this.state.episodeAirDates[i].slice(-2)}, {' ' + this.state.episodeAirDates[i].slice(0, 4)}
-								</Text> :
-								<Text style={styles.airDate}>
-									Aired on
+										{' ' + this.state.episodeAirDates[i].slice(-2)}, {' ' + this.state.episodeAirDates[i].slice(0, 4)}
+									</Text> :
+									<Text style={styles.airDate}>
+										Aired on
 									{' ' + this.monthNames[new Date(this.state.episodeAirDates[i]).getMonth()]}
-									{' ' + this.state.episodeAirDates[i].slice(-2)}, {' ' + this.state.episodeAirDates[i].slice(0, 4)}
-								</Text>
-							) : null
-					}
-					{
-						this.state.episodeOverviews[i] !== null ?
-						<Text style={styles.overview}>
-							{this.state.episodeOverviews[i]}
-						</Text> : null
-					}
-					{
-						<View style={styles.voteWrapper}>
-							<Text style={styles.text}>{this.state.episodeVoteAverages[i]}</Text>
-							<Icon name="heart" size={15} color="#db0a5b" style={styles.heartIcon}/>
-							<Text style={styles.text}>by {this.state.episodeVoteCounts[i]} {this.state.episodeVoteCounts[i] > 1 ? 'people' : 'person'}</Text>
-						</View>
-					}
-				</View>
-			);
-		}
+										{' ' + this.state.episodeAirDates[i].slice(-2)}, {' ' + this.state.episodeAirDates[i].slice(0, 4)}
+									</Text>
+								) : null
+						}
+						{
+							this.state.episodeOverviews[i] !== null ?
+								<Text style={styles.overview}>
+									{this.state.episodeOverviews[i]}
+								</Text> : null
+						}
+						{
+							<View style={styles.voteWrapper}>
+								<Text style={styles.text}>{this.state.episodeVoteAverages[i]}</Text>
+								<Icon name="heart" size={15} color="#db0a5b" style={styles.heartIcon} />
+								<Text style={styles.text}>by {this.state.episodeVoteCounts[i]} {this.state.episodeVoteCounts[i] > 1 ? 'people' : 'person'}</Text>
+							</View>
+						}
+					</View>
+				);
+			}
 
-		return (
-			<ImageBackground blurRadius={1.5}
-				source={require('../assets/lilypads.png')}
-				resizeMode="cover" style={styles.bgImage}>
+			let contentJsx =
 				<ScrollView style={styles.container}>
 					<ScrollView>
 						<View style={styles.seasonContainer}>
@@ -168,11 +129,67 @@ export default class SeasonDetailsScreen extends Component {
 							{airDateJsx}
 						</View>
 					</ScrollView>
-
 					<ScrollView horizontal={true}>
 						{episodesJsx}
 					</ScrollView>
-				</ScrollView>
+				</ScrollView>;
+			this.setState({ contentJsx });
+		};
+
+		this.fetchSeasonDetails = (titleId, seasonNo) => {
+			return new Promise((resolve, reject) => {
+				this.setState({
+					contentJsx: <ActivityIndicator size="large" color="#22a7f0" style={styles.indicator} />,
+				}, () => {
+					fetch(`https://api-cine-digest.herokuapp.com/api/v1/gets/${titleId}/${seasonNo}`)
+						.then(response => response.json())
+						.then(jsonResponse => {
+							this.episodes = [];
+							this.episodeAirDates = [];
+							this.episodeNames = [];
+							this.episodeOverviews = [];
+							this.episodeVoteAverages = [];
+							this.episodeVoteCounts = [];
+							this.noPoster = jsonResponse.poster_path !== null ? false : true;
+							for (let i = 0; i < jsonResponse.episodes.length; ++i) {
+								this.episodes.push(jsonResponse.episodes[i].episode_number);
+								this.episodeAirDates.push(jsonResponse.episodes[i].air_date);
+								this.episodeNames.push(jsonResponse.episodes[i].name);
+								this.episodeOverviews.push(jsonResponse.episodes[i].overview);
+								this.episodeVoteAverages.push(jsonResponse.episodes[i].vote_average);
+								this.episodeVoteCounts.push(jsonResponse.episodes[i].vote_count);
+							}
+							this.setState({
+								posterPath: `https://image.tmdb.org/t/p/original/${jsonResponse.poster_path}`,
+								seasonNo: jsonResponse.season_number,
+								airDate: jsonResponse.air_date,
+								episodes: this.episodes,
+								episodeAirDates: this.episodeAirDates,
+								episodeNames: this.episodeNames,
+								episodeOverviews: this.episodeOverviews,
+								episodeVoteAverages: this.episodeVoteAverages,
+								episodeVoteCounts: this.episodeVoteCounts,
+							});
+							this.initScreen()
+								.then(() => resolve(true))
+								.catch(error => reject(error));
+						});
+				});
+			});
+		};
+	}
+
+	componentDidMount() {
+		this.fetchSeasonDetails(this.titleId, this.seasonNo)
+			.catch(error => console.warn(error));
+	}
+
+	render() {
+		return (
+			<ImageBackground blurRadius={1.5}
+				source={require('../assets/lilypads.png')}
+				resizeMode="cover" style={styles.bgImage}>
+				{this.state.contentJsx}
 			</ImageBackground>
 		);
 	}
@@ -258,5 +275,10 @@ const styles = StyleSheet.create(
         marginLeft: 5,
         marginRight: 5,
         marginBottom: 13,
-    },
+	},
+	indicator: {
+		flex: 1,
+		alignSelf: 'center',
+		margin: 20,
+	},
 });
