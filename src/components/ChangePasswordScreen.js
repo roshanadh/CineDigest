7278cdd7839dcb1f60f9934d483e32d48868e93c
Roshan Adhikari <nahsorad@gmail.com>
2019-08-28 17:54:15 +0545

    Style ChangePasswordScreen

--- src/components/ChangePasswordScreen.js
+++ src/components/ChangePasswordScreen.js
@@ -8,8 +8,10 @@ import {
     ActivityIndicator,
     ScrollView,
     ImageBackground,
+    ProgressBarAndroid,
 } from 'react-native';
 import KeyIcon from 'react-native-vector-icons/Feather';
+import MCIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 export default class ChangePassword extends Component {
     static navigationOptions = ({ navigation }) => {
@@ -28,9 +30,118 @@ export default class ChangePassword extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            passwordProgress: 0,
             oldPassword: '',
             newPassword: '',
             passwordConfirmation: '',
+        };
+
+        this.updatePassword = (newPassword) => {
+            if (this.state.newPassword < newPassword) {
+                // Password length increased
+                if (newPassword.length >= 8) {
+                    this.setState({ newPassword, passwordProgress: 1 });
+                } else {
+                    this.setState({ newPassword, passwordProgress: this.state.passwordProgress + (1 / 8) });
+                }
+            } else {
+                // Password length decreased
+                if (newPassword.length === 0) {
+                    this.setState({ newPassword, passwordProgress: 0 });
+                } else if (newPassword.length >= 8) {
+                    this.setState({ newPassword, passwordProgress: 1 });
+                } else {
+                    this.setState({ newPassword, passwordProgress: this.state.passwordProgress - (1 / 8) });
+                }
+            }
+        };
+
+        this.genProgressBarJsx = () => {
+            if (this.state.newPassword.length === 0) {
+                return (
+                    <ProgressBarAndroid
+                        styleAttr="Horizontal"
+                        indeterminate={false}
+                        progress={1}
+                        style={styles.progressBar}
+                        color="#22a7f0" />
+                );
+            } else if (this.state.newPassword.length > 0 && this.state.newPassword.length < 6) {
+                return (
+                    <ProgressBarAndroid
+                        styleAttr="Horizontal"
+                        indeterminate={false}
+                        progress={this.state.passwordProgress}
+                        style={styles.progressBar}
+                        color="#e74c3c" />
+                );
+            } else if (this.state.newPassword.length >= 6 && this.state.newPassword.length < 8) {
+                return (
+                    <ProgressBarAndroid
+                        styleAttr="Horizontal"
+                        indeterminate={false}
+                        progress={this.state.passwordProgress}
+                        style={styles.progressBar}
+                        color="#f4b350" />
+                );
+            } else {
+                return (
+                    <ProgressBarAndroid
+                        styleAttr="Horizontal"
+                        indeterminate={false}
+                        progress={this.state.passwordProgress}
+                        style={styles.progressBar}
+                        color="#22a7f0" />
+                );
+            }
+        };
+
+        this.genOldPasswordIconJsx = () => {
+            if (this.state.oldPassword.length === 0) {
+                return (
+                    <KeyIcon name="key" size={25} color="#ddd" />
+                );
+            } else {
+                return (
+                    <KeyIcon name="key" size={25} color="#22a7f0" />
+                );
+            }
+        };
+
+        this.genPasswordIconJsx = () => {
+            if (this.state.newPassword.length === 0) {
+                return (
+                    <KeyIcon name="key" size={25} color="#ddd" />
+                );
+            } else if (this.state.newPassword.length > 0 && this.state.newPassword.length < 6) {
+                return (
+                    <KeyIcon name="key" size={25} color="#e74c3c" />
+                );
+            } else if (this.state.newPassword.length >= 6 && this.state.newPassword.length < 8) {
+                return (
+                    <KeyIcon name="key" size={25} color="#f4b350" />
+                );
+            } else {
+                return (
+                    <KeyIcon name="key" size={25} color="#22a7f0" />
+                );
+            }
+        };
+
+        this.genConfirmPasswordIconJsx = () => {
+            if (this.state.passwordConfirmation.length === 0) {
+                return (
+                    <KeyIcon name="key" size={25} color="#ddd" />
+                );
+            } else if (this.state.passwordConfirmation.length > 0 && (this.state.newPassword !== this.state.passwordConfirmation)) {
+                return (
+                    <KeyIcon name="key" size={25} color="#e74c3c" />
+                );
+            } else {
+                return (
+                    <KeyIcon name="key" size={25} color="#22a7f0" />
+                );
+            }
         };
     }
 
@@ -41,54 +152,75 @@ export default class ChangePassword extends Component {
         let newPasswordLengthErrorTextJsx =
             this.state.newPassword.length > 0 && this.state.newPassword.length < 6 ?
                 <Text style={styles.errorText}>Password must contain atleast 6 characters</Text> : null;
-        let confirmPasswordErrorTextJsx =
+        let confirmPasswordLengthErrorTextJsx =
             this.state.passwordConfirmation.length > 0 && (this.state.newPassword !== this.state.passwordConfirmation) ?
-                <Text style={styles.errorText}>Passwords do not match</Text> : <View style={styles.emptyView} />;
-
+                <Text style={styles.errorText}>Passwords do not match</Text> : null;
+        let progressBarJsx = this.genProgressBarJsx();
+        let oldPasswordIconJsx = this.genOldPasswordIconJsx();
+        let passwordIconJsx = this.genPasswordIconJsx();
+        let confirmPasswordIconJsx = this.genConfirmPasswordIconJsx();
 
         return (
             <ImageBackground blurRadius={1.3}
                 source={require('../assets/lilypads.png')}
                 resizeMode="cover" style={styles.bgImage}>
                 <ScrollView style={styles.scrollView}>
-                    <View style={styles.textInputWrapper}>
-                        <TextInput placeholder="Old Password"
-                            secureTextEntry={true}
-                            editable={this.state.isPasswordEditable}
-                            style={styles.textInput}
-                            autoCapitalize="none"
-                            onChangeText={oldPassword => this.setState({ oldPassword })}
-                            returnKeyType="next" />
-                        <KeyIcon name="key" size={25} color="#ddd" />
-                    </View>
-                    <View style={styles.textInputWrapper}>
-                        <TextInput placeholder="New Password"
-                            secureTextEntry={true}
-                            editable={this.state.isPasswordEditable}
-                            style={styles.textInput}
-                            autoCapitalize="none"
-                            onChangeText={newPassword => this.setState({ newPassword })}
-                            returnKeyType="next" />
-                        <KeyIcon name="key" size={25} color="#ddd" />
-                    </View>
-                    <View style={styles.textInputWrapper}>
-                        <TextInput placeholder="Confirm New Password"
-                            secureTextEntry={true}
-                            editable={this.state.isPasswordEditable}
-                            style={styles.textInput}
-                            autoCapitalize="none"
-                            onChangeText={passwordConfirmation => this.setState({ passwordConfirmation })}
-                            returnKeyType="next" />
-                        <KeyIcon name="key" size={25} color="#ddd" />
-                    </View>
-                    <TouchableOpacity style={styles.changePassBtn}
-                        onPress={() => console.warn('Pressed!')}>
-                        <Text style={styles.btnText}>Change Password</Text>
-                        {indicatorJsx}
-                    </TouchableOpacity>
-                    <View style={styles.footer}>
-                        {newPasswordLengthErrorTextJsx}
-                        {confirmPasswordErrorTextJsx}
+                    <View style={styles.container}>
+                        <View style={styles.changePasswordContainer}>
+                            <MCIcons name="textbox-password" size={80} color="#34495e" style={styles.passwordIcon} />
+                            <View style={styles.metaWrapper}>
+                                <View style={styles.passwordWrapper}>
+                                    <TextInput placeholder="Old Password"
+                                        secureTextEntry={true}
+                                        style={styles.textInput}
+                                        autoCapitalize="none"
+                                        onChangeText={oldPassword => this.setState({ oldPassword })}
+                                        returnKeyType="next" />
+                                    {oldPasswordIconJsx}
+                                </View>
+                            </View>
+                            <View style={styles.metaWrapper}>
+                                <View style={styles.noBorderPasswordWrapper}>
+                                    <TextInput placeholder="New Password"
+                                        secureTextEntry={true}
+                                        editable={this.state.isPasswordEditable}
+                                        style={styles.textInput}
+                                        autoCapitalize="none"
+                                        onChangeText={newPassword => this.updatePassword(newPassword)}
+                                        returnKeyType="next" />
+                                    {passwordIconJsx}
+                                </View>
+                                {progressBarJsx}
+                            </View>
+                            <View style={styles.metaWrapper}>
+                                <View style={
+                                    confirmPasswordLengthErrorTextJsx !== null ?
+                                        styles.errorWrapper : styles.passwordWrapper
+                                }>
+                                    <TextInput placeholder="Confirm New Password"
+                                        secureTextEntry={true}
+                                        editable={this.state.isPasswordEditable}
+                                        style={styles.textInput}
+                                        autoCapitalize="none"
+                                        onChangeText={passwordConfirmation => this.setState({ passwordConfirmation })}
+                                        returnKeyType="next" />
+                                    {confirmPasswordIconJsx}
+                                </View>
+                            </View>
+                            <View style={styles.footer}>
+                                {newPasswordLengthErrorTextJsx}
+                                {confirmPasswordLengthErrorTextJsx}
+                            </View>
+                            <TouchableOpacity style={styles.changePassBtn}
+                                onPress={() => console.warn('Pressed!')}>
+                                <Text style={styles.btnText}>Change Password</Text>
+                                {indicatorJsx}
+                            </TouchableOpacity>
+                        </View>
+                        <View style={styles.infoContainer}>
+                            <Text style={styles.infoText}>You need a password to access your lists stored on this device.</Text>
+                            <Text style={styles.infoText}>Create a strong password that comprises different $ymb0ls and cAsEs.</Text>
+                        </View>
                     </View>
                 </ScrollView>
             </ImageBackground>
@@ -102,27 +234,83 @@ const styles = StyleSheet.create({
         height: '100%',
         flex: 1,
     },
-    scrollView: {
-        marginTop: 50,
-        paddingLeft: 20,
-        paddingRight: 20,
+    container: {
+        paddingTop: 10,
+        paddingBottom: 10,
     },
-    textInputWrapper: {
+    changePasswordContainer: {
+        flexDirection: 'column',
+        justifyContent: 'flex-start',
+        padding: 20,
+        flex: 1,
+        alignItems: 'center',
+        backgroundColor: '#fff',
+        marginBottom: 10,
+        borderWidth: 0.1,
+        borderColor: '#013243',
+    },
+    infoContainer: {
+        borderWidth: 0.1,
+        borderColor: '#013243',
+        flexDirection: 'column',
+        padding: 20,
+        paddingBottom: 0,
+        flex: 1,
+        width: '100%',
+        backgroundColor: '#fff',
+    },
+    passwordIcon: {
+        margin: 25,
+        color: '#34495e',
+    },
+    metaWrapper: {
+        flexDirection: 'column',
+        justifyContent: 'flex-start',
+        alignItems: 'flex-start',
+        marginBottom: 25,
+        width: '100%',
+    },
+    passwordWrapper: {
         flexDirection: 'row',
         justifyContent: 'center',
         alignItems: 'center',
         borderBottomWidth: 1,
-        borderColor: 'rgba(103, 128, 159, 0.5)',
+        borderColor: '#22a7f0',
         paddingLeft: 20,
         paddingRight: 20,
-        marginBottom: 25,
+        backgroundColor: 'rgba(255,255,255,0.3)',
+    },
+    noBorderPasswordWrapper: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingLeft: 20,
+        paddingRight: 20,
+        backgroundColor: 'rgba(255,255,255,0.3)',
+    },
+    progressBar: {
+        width: '100%',
+        height: 1.2,
+    },
+    horizontalRule: {
+        borderBottomWidth: 1,
+        width: '100%',
+        borderColor: '#22a7f0',
+    },
+    errorWrapper: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderBottomWidth: 1,
+        borderColor: '#e74c3c',
+        paddingLeft: 20,
+        paddingRight: 20,
         backgroundColor: 'rgba(255,255,255,0.3)',
     },
     textInput: {
         marginRight: 10,
         flex: 5,
         minHeight: '6%',
-        opacity: 1,
     },
     changePassBtn: {
         flexDirection: 'row',
@@ -130,9 +318,8 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         alignSelf: 'center',
         borderRadius: 50,
-        marginTop: 20,
         padding: 15,
-        width: '60%',
+        width: '70%',
         backgroundColor: '#22a7f0',
     },
     btnText: {
@@ -143,7 +330,7 @@ const styles = StyleSheet.create({
         marginLeft: 20,
     },
     footer: {
-        marginTop: 30,
+        marginBottom: 10,
     },
     errorText: {
         color: '#e74c3c',
@@ -152,8 +339,10 @@ const styles = StyleSheet.create({
         textAlign: 'center',
         marginBottom: 10,
     },
-    emptyView: {
-        // marginTop: 30,
-        marginBottom: 10,
+    infoText: {
+        fontSize: 13,
+        color: '#34495e',
+        textAlign: 'center',
+        marginBottom: 15,
     },
 });
