a005bd943ff898aeb47e660de6f5b856006d4bdb
Roshan Adhikari <nahsorad@gmail.com>
2019-09-01 16:23:05 +0545

    User multiset function to set multiple key-values

--- src/auth/auth.js
+++ src/auth/auth.js
@@ -3,8 +3,9 @@ import AsyncStorage from '@react-native-community/async-storage';
 export const USER_KEY = 'auth-demo-key'; // Store this key in AsyncStorage
 
 export const onSignIn = (username, uuid) => {
-    AsyncStorage.setItem('USER_KEY', username);
-    AsyncStorage.setItem('UUID', uuid);
+    return new Promise((resolve, reject) => {
+        AsyncStorage.multiSet([['USER_KEY', username], ['UUID', uuid]], () => resolve(true));
+    });
 };
 
 export const onSignOut = () => AsyncStorage.removeItem('USER_KEY');
