4307a50ec24820769f96c0c25dd0145bacfdf58f
Roshan Adhikari <nahsorad@gmail.com>
2019-09-28 17:28:43 +0545

    Set notification action OK

--- src/components/MovieDetailsScreen.js
+++ src/components/MovieDetailsScreen.js
@@ -606,7 +606,7 @@ export default class MovieDetails extends Component {
                 soundName: 'default', // (optional) Sound to play when the notification is shown. Value of 'default' plays the default sound. It can be set to a custom sound such as 'android.resource://com.xyz/raw/my_sound'. It will look for the 'my_sound' audio file in 'res/raw' directory and play it. default: 'default' (default sound is played)
                 number: '10', // (optional) Valid 32 bit integer specified as string. default: none (Cannot be zero)
                 repeatType: 'day', // (optional) Repeating interval. Check 'Repeating Notifications' section for more info.
-                actions: '["Yes", "No"]',  // (Android only) See the doc for notification actions to know more
+                actions: '["OK"]',  // (Android only) See the doc for notification actions to know more
                 date: new Date(new Date(notifyYear, notifyMonth, notifyDay, 6, 20, 0).getTime()),
             });
             Alert.alert(
