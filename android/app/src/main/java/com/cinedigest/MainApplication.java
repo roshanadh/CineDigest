c047bd641b4980b6fc1ee2dad2258eb0fd3bf713
Roshan Adhikari <nahsorad@gmail.com>
2019-09-28 11:21:24 +0545

    Install datetimepicker

--- android/app/src/main/java/com/cinedigest/MainApplication.java
+++ android/app/src/main/java/com/cinedigest/MainApplication.java
@@ -7,6 +7,7 @@ import com.facebook.react.PackageList;
 import com.facebook.hermes.reactexecutor.HermesExecutorFactory;
 import com.facebook.react.bridge.JavaScriptExecutorFactory;
 import com.facebook.react.ReactApplication;
+import com.reactcommunity.rndatetimepicker.RNDateTimePickerPackage;
 import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.facebook.react.ReactNativeHost;
